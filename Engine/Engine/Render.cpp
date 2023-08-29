bce09b747db548b94bb18c73284272c94f4f0507
a <matiasmonti2011@hotmail.com>
2023-08-29 12:02:19 -0300

    Change Draw to Index Based Draw

--- Engine/Engine/Render.cpp
+++ Engine/Engine/Render.cpp
@@ -1,5 +1,45 @@
 #include "Render.h"
+#include <fstream>
 #include <iostream>
+#include <sstream>
+
+ShaderProgramSource Render::ParseShader(const std::string& filePath)
+{
+    std::ifstream stream(filePath);
+
+    enum class ShaderType
+    {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    std::string line;
+
+    std::stringstream ss[2];
+
+    ShaderType type = ShaderType::NONE;
+
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+            {
+                type = ShaderType::VERTEX;
+            }
+            else if (line.find("fragment") != std::string::npos)
+            {
+                type = ShaderType::FRAGMENT;
+            }
+        }
+        else
+        {
+            ss[(int)type] << line << '\n';
+        }
+
+    }
+
+    return { ss[0].str(), ss[1].str() };
+}
 
 void Render::ClearScreen()
 {
@@ -15,45 +55,39 @@ void Render::PostRender(GLFWwindow* window)
 
 void Render::DrawTriangle(float x, float y)
 {
-    float positions[6] = {
+    float positions[] = {
         -0.5f + x, -0.5f + y,
-         0.0f + x, 0.5f + y,
-         0.5f + x, -0.5f + y
+         0.5f + x, -0.5f + y,
+         0.5f + x, 0.5f + y,
+         -0.5f + x, 0.5f + y,
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * 2 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-    std::string vertexShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;"
-        "\n"
-        "void main() \n"
-        "{\n"
-        "   gl_Position = position; \n"
-        "} \n";
+    ShaderProgramSource source = ParseShader("Basic.shader");
 
-    std::string fragmentShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) out vec4 color;"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "   color = vec4(1.0, 0.0, 0.0, 1.0); \n"
-        "}\n";
-
-    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    //CUALQUIER INDEX BUFFER ESTA HECHO DE UNSIGNED INTS
+    //SI SE CAMBIA A INT SE ROMPE
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
@@ -104,4 +138,10 @@ unsigned int Render::CreateShader(const std::string& vertexShader, const std::st
     glDeleteShader(fs);
 
     return program;
+}
+
+void Render::DeleteShader()
+{
+    //TO DO
+    //glDeleteProgram(shader);
 }
--- Engine/Engine/Render.h
+++ Engine/Engine/Render.h
@@ -2,15 +2,23 @@
 #include "Window.h"
 #include <string>
 
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
 class Render
 {
 private:
-
+	
 public:
+	static ShaderProgramSource ParseShader(const std::string& filePath);
 	void ClearScreen();
 
 	void PostRender(GLFWwindow* window);
 	void DrawTriangle(float x, float y);
 	static unsigned int CompilerShader(unsigned int type, const std::string& source);
 	static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+	void DeleteShader();
 };
