ebb6fe191b54706d4feabd9eb2b23ea8d46a83fe
nicorm2000 <nicorm2000@gmail.com>
2023-09-26 13:35:32 -0300

    Default state of the game

--- Engine/Game/main.cpp
+++ Engine/Game/main.cpp
@@ -4,21 +4,14 @@
 class Game : public BaseGame
 {
 	Shape* shape = nullptr;
-	glm::vec3 red = { 1, 0, 0 };
 
 	void Start()
 	{
 		shape = new Shape(renderer);
-		shape->CreateTriangle();
-		shape->SetColor(red);
-		shape->SetScale(100, 100, 1);
-		shape->SetPosition(0, 0, 0);
 	}
 
 	void Update()
 	{
-		shape->SetPosition(5, 5, 0);
-		shape->SetRotationZ(10);
 		shape->Draw();
 	}
 
