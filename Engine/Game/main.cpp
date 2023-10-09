90c0b2fffcb71bdb5d4aba32935438ef26fbd869
Matias <matiasmonti2011@hotmail.com>
2023-10-08 22:50:47 -0300

    Modify lib

--- Engine/Game/main.cpp
+++ Engine/Game/main.cpp
@@ -1,30 +1,5 @@
 #include <iostream>
-#include "BaseGame/BaseGame.h"
-
-class Game : public BaseGame
-{
-	Shape* shape = nullptr;
-	glm::vec3 red = { 1, 0, 0 };
-
-	void Start()
-	{
-		shape = new Shape(renderer);
-		shape->CreateTriangle();
-		shape->SetColor(red);
-		shape->SetScale(100, 100, 1);
-		shape->SetPosition(0, 0, 0);
-	}
-
-	void Update()
-	{
-		shape->Draw();
-	}
-
-	void End()
-	{
-		delete shape;
-	}
-};
+#include "Game/Game.h"
 
 int main()
 {
