091ffd9ad58ebbbcec8a3a9ee2716b9014bd5fef
Matias <matiasmonti2011@hotmail.com>
2023-10-13 18:10:47 -0300

    Add CollisionManager

---
+++ Engine/Engine/src/CollisionManager/CollisionManager.h
@@ -0,0 +1,25 @@
+#ifndef COLLISIONMANAGER_H
+#define COLLISIONMANAGER_H
+
+#include "Entity2D/Entity2D.h"
+#include "Export.h"
+
+class DllApi CollisionManager
+{
+private:
+	Entity2D* entity2D;
+
+	const float collisionMoveSpeed = 0.001f;
+
+public:
+	CollisionManager();
+	~CollisionManager();
+
+	static void CollisionUpdate(Entity2D* entity1, Entity2D* entity2);
+	static bool CheckCollision(Entity2D* entity1, Entity2D* entity2);
+	static void Overlap(Entity2D* entity1, Entity2D* entity2);
+
+	static bool CheckCollisionTile(Entity2D* entity1, Entity2D* entity2);
+};
+
+#endif
