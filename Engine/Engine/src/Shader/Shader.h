ba46934033dee7adc69fa9aa18f49708af53ea2f
nicorm2000 <nicorm2000@gmail.com>
2024-04-24 19:06:53 -0300

    Added shader class

--- Engine/Engine/src/Shader/Shader.h
+++ Engine/Engine/src/Shader/Shader.h
@@ -1,1 +1,30 @@
-#pragma once
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "Export.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+class DllApi Shader
+{
+private:
+	unsigned int shaderId;
+
+	void VertexShader(string vertexPath);
+	void FragmentShader(string fragmentPath);
+	void LinkShader();
+	string ReadShaderFile(string path);
+
+public:
+	Shader();
+	~Shader();
+
+	void CreateShader(string vertexPath, string fragmentPath);
+	unsigned int GetShaderId();
+};
+
+#endif
